cf208cb3b23ba6b1aa24243b6e81ce7a0317ea3f
Ivan_XZ <xz1051708973@163.com>
2021-03-03 17:45:52 +0800

    feat: add 283-moveZeroes

---
+++ src/leetcode/283-moveZeroes/main.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 谢卓 on 2021/3/3.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if(nums[i] != 0 ) {
+                if(i != k)
+                    swap(nums[i], nums[k++]);
+                else
+                    k++;
+            }
+        }
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution solution;
+    vector<int> nums = {0,1,0,3,12};
+    solution.moveZeroes(nums);
+
+    for(auto n: nums) {
+        cout << n << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ src/test/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 谢卓 on 2021/3/3.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
