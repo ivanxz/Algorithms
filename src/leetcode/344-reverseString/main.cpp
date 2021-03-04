114b8cb6060bbe23c80308de0034669ea80fd939
Ivan_XZ <xz1051708973@163.com>
2021-03-04 20:21:37 +0800

    feat: add 344-reverseString

--- src/leetcode/344-reverseString/main.cpp
+++ src/leetcode/344-reverseString/main.cpp
@@ -9,10 +9,20 @@ using namespace std;
 
 class Solution {
 public:
-    
+    static void reverseString(vector<char> &s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            swap(s[l++], s[r--]);
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {
-
+    vector<char> s = {'H', 'a', 'n', 'n', 'a', 'h'};
+    Solution::reverseString(s);
+    for (auto c: s) {
+        cout << c << " ";
+    }
+    cout << endl;
     return 0;
 }
