141ef1d7c7bbd7afd57e643b2074fc01518a69cb
Ivan_XZ <xz1051708973@163.com>
2021-03-04 20:15:24 +0800

    feat: add 125-isPalindrome

--- src/leetcode/125-isPalindrome/main.cpp
+++ src/leetcode/125-isPalindrome/main.cpp
@@ -9,10 +9,31 @@ using namespace std;
 
 class Solution {
 public:
+    bool isPalindrome(string s) {
+        string newStr;
+        for (auto c: s) {
+            if (isalnum(c)) {
+                newStr.push_back(tolower(c));
+            }
+        }
 
+        int l = 0, r = newStr.size() - 1;
+        while (l < r) {
+            if (newStr[l] != newStr[r]) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
 };
 
 int main(int argc, char *argv[]) {
-
+    Solution solution;
+    string s = "A man, a plan, a canal: Panama";
+    cout << solution.isPalindrome(s) << endl;
     return 0;
 }
