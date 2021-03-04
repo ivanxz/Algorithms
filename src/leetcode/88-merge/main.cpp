ea783085adb51bdd60f2fff54248defbe1d69aa5
Ivan_XZ <xz1051708973@163.com>
2021-03-04 15:35:51 +0800

    modify: add 88-merge

--- src/leetcode/88-merge/main.cpp
+++ src/leetcode/88-merge/main.cpp
@@ -13,17 +13,17 @@ public:
         int i = m - 1, j = n - 1, k = m + n - 1;
         while (j >= 0) {
             if (i >= 0 && nums1[i] > nums2[j])
-                nums1[k] = nums1[i--];
+                nums1[k--] = nums1[i--];
             else
-                nums1[k] = nums2[j--];
+                nums1[k--] = nums2[j--];
         }
     }
 };
 
 int main(int argc, char *argv[]) {
-    vector<int> nums1 = {0};
-    vector<int> nums2 = {1};
-    Solution::merge(nums1, 0, nums2, 1);
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+    Solution::merge(nums1, 3, nums2, 3);
     for (auto k: nums1) {
         cout << k << " ";
     }
