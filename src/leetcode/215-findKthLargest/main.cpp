91895be59b8b8b414315ec07a28bb097dbddec5a
Ivan_XZ <xz1051708973@163.com>
2021-03-04 16:53:16 +0800

    feat: add 215-findKthLargest

--- src/leetcode/215-findKthLargest/main.cpp
+++ src/leetcode/215-findKthLargest/main.cpp
@@ -9,10 +9,43 @@ using namespace std;
 
 class Solution {
 public:
+    int findKthLargest(vector<int>& nums, int k) {
+        srand(time(nullptr));
+        auto n = nums.size();
+        return quickSelect(nums, 0, n - 1, n - k);
+    }
 
+private:
+    inline int partition(vector<int>& a, int l, int r) {
+        int x = a[r], i = l - 1;
+        for (int j = l; j < r; ++j) {
+            if (a[j] <= x) {
+                swap(a[++i], a[j]);
+            }
+        }
+        swap(a[i + 1], a[r]);
+        return i + 1;
+    }
+
+    inline int randomPartition(vector<int>& a, int l, int r) {
+        int i = rand() % (r - l + 1) + l;
+        swap(a[i], a[r]);
+        return partition(a, l, r);
+    }
+
+    inline int quickSelect(vector<int>& a, int l, int r, int index) {
+        int q = randomPartition(a, l, r);
+        if (q == index)
+            return a[q];
+        else
+            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
+    }
 };
 
 int main(int argc, char *argv[]) {
-    cout << endl;
+    vector<int> nums = {3,2,3,1,2,4,5,5,6};
+    int k = 4;
+    Solution solution;
+    cout << solution.findKthLargest(nums, k) << endl;
     return 0;
 }
