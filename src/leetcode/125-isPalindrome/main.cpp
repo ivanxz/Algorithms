4555f6cee1358986b75b3b0b694f21d3bcfede11
Ivan_XZ <xz1051708973@163.com>
2021-03-04 17:39:35 +0800

    feat: add 167-twoSum

---
+++ src/leetcode/125-isPalindrome/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 谢卓 on 2021/3/4.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+};
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
--- src/leetcode/167-twoSum/main.cpp
+++ src/leetcode/167-twoSum/main.cpp
@@ -9,12 +9,28 @@ using namespace std;
 
 class Solution {
 public:
-    static vector<int> twoSum(vector<int>& numbers, int target) {
+    static vector<int> twoSum(vector<int> &numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target)
+                break;
+            else if (numbers[l] + numbers[r] > target)
+                r--;
+            else
+                l++;
+        }
 
+        return {l + 1, r + 1};
     }
 };
 
 int main(int argc, char *argv[]) {
+    vector<int> numbers = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> ret = Solution::twoSum(numbers, target);
+    for (auto n: ret) {
+        cout << n << " ";
+    }
     cout << endl;
     return 0;
 }
