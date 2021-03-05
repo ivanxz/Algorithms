2ba2b18c0929a073f706583d592233acacf7a9c5
Ivan_XZ <xz1051708973@163.com>
2021-03-05 11:42:38 +0800

    feat: add 11-maxArea

--- src/leetcode/11-maxArea/main.cpp
+++ src/leetcode/11-maxArea/main.cpp
@@ -9,10 +9,26 @@ using namespace std;
 
 class Solution {
 public:
-    
+    static int maxArea(vector<int> &height) {
+        int l = 0, r = height.size() - 1;
+        int ans = 0, area;
+        while (l < r) {
+            area = min(height[l], height[r]) * (r - l);
+            ans = max(ans, area);
+
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return ans;
+    }
 };
 
 int main(int argc, char *argv[]) {
-
+    vector<int> height = {4, 3, 2, 1, 4};
+    cout << Solution::maxArea(height) << endl;
     return 0;
 }
