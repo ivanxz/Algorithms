6de37644ba042b25e6cd2e36978683200a73ca96
Ivan_XZ <xz1051708973@163.com>
2021-03-05 11:10:45 +0800

    feat: add 345-reverseVowels

--- src/leetcode/345-reverseVowels/main.cpp
+++ src/leetcode/345-reverseVowels/main.cpp
@@ -9,10 +9,45 @@ using namespace std;
 
 class Solution {
 public:
-    
+    static string reverseVowels(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (!isVowels(s[l])) {
+                l++;
+            } else if (!isVowels(s[r])) {
+                r--;
+            } else {
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }
+        }
+
+        return s;
+    }
+
+private:
+    static bool isVowels(char c) {
+        switch (c) {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {
-
+    string s = "hello";
+    cout << Solution::reverseVowels(s) << endl;
     return 0;
 }
