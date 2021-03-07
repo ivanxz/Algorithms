37f8ba32d6ae7a6782d7d536e30c9ad3dc164786
Ivan_XZ <xz1051708973@163.com>
2021-03-07 19:42:28 +0800

    feat: add 206-reverseList 92-reverseBetween

--- src/leetcode/9999-template/main.cpp
+++ src/leetcode/9999-template/main.cpp
@@ -1,11 +1,22 @@
 //
-// Created by 谢卓 on 2021/3/6.
+// Created by 谢卓 on 2021/3/7.
 //
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
---
+++ src/leetcode/list/206-reverseList/main.cpp
@@ -0,0 +1,83 @@
+//
+// Created by 谢卓 on 2021/3/7.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(int arr[], int n) {
+    if (n == 0)
+        return nullptr;
+
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *curNode = head;
+    for (int i = 1; i < n; ++i) {
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        cout << curNode->val << " -> ";
+        curNode = curNode->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+void deleteListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        ListNode* delNode = curNode;
+        curNode = curNode->next;
+        delete delNode;
+    }
+}
+
+class Solution {
+public:
+    ListNode *reverseList(ListNode *head) {
+        ListNode *pre = nullptr;
+        ListNode *cur = head;
+
+        while (cur != nullptr) {
+            ListNode *next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+
+    ListNode *head = createListNode(arr, n);
+    printListNode(head);
+
+    ListNode *retNode = Solution().reverseList(head);
+    printListNode(retNode);
+
+    deleteListNode(retNode);
+    return 0;
+}
