612a2cda8cee13118c5d0168e4db286c7f8cb9ef
Ivan_XZ <xz1051708973@163.com>
2021-03-07 21:19:42 +0800

    feat: add 86-partition

---
+++ src/leetcode/list/86-partition/main.cpp
@@ -0,0 +1,99 @@
+//
+// Created by 谢卓 on 2021/3/7.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(int arr[], int n) {
+    if (n == 0)
+        return nullptr;
+
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *curNode = head;
+    for (int i = 1; i < n; ++i) {
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        cout << curNode->val << " -> ";
+        curNode = curNode->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+void deleteListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        ListNode *delNode = curNode;
+        curNode = curNode->next;
+        delete delNode;
+    }
+}
+
+class Solution {
+public:
+    ListNode *partition(ListNode *head, int x) {
+        ListNode *dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+
+        ListNode *preNode = dummyNode;
+        while (preNode->next != nullptr && preNode->next->val < x) {
+            preNode = preNode->next;
+        }
+
+        ListNode *curNode = preNode;
+        ListNode *nextNode;
+        while (curNode != nullptr && curNode->next != nullptr) {
+            if (curNode->next->val < x) {
+                nextNode = curNode->next;
+                curNode->next = nextNode->next;
+                nextNode->next = preNode->next;
+                preNode->next = nextNode;
+
+                preNode = preNode->next;
+            } else {
+                curNode = curNode->next;
+            }
+        }
+
+        ListNode *retNode = dummyNode->next;
+        delete dummyNode;
+
+        return retNode;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    int arr[] = {1, 4, 3, 2, 5, 2};
+    int n = sizeof(arr) / sizeof(int);
+
+    ListNode *head = createListNode(arr, n);
+    printListNode(head);
+
+    ListNode *retNode = Solution().partition(head, 3);
+    printListNode(retNode);
+
+    deleteListNode(retNode);
+    return 0;
+}
