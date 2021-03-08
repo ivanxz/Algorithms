e6de29b126d3f5e4f91aa41f8c03b8048a31a9e6
Ivan_XZ <xz1051708973@163.com>
2021-03-08 15:02:09 +0800

    feat: add 147-insertionSortList

---
+++ src/leetcode/list/147-insertionSortList/main.cpp
@@ -0,0 +1,96 @@
+//
+// Created by 谢卓 on 2021/3/7.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *createListNode(int arr[], int n) {
+    if (n == 0)
+        return nullptr;
+
+    ListNode *head = new ListNode(arr[0]);
+    ListNode *curNode = head;
+    for (int i = 1; i < n; ++i) {
+        curNode->next = new ListNode(arr[i]);
+        curNode = curNode->next;
+    }
+
+    return head;
+}
+
+void printListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        cout << curNode->val << " -> ";
+        curNode = curNode->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+void deleteListNode(ListNode *head) {
+    ListNode *curNode = head;
+    while (curNode != nullptr) {
+        ListNode* delNode = curNode;
+        curNode = curNode->next;
+        delete delNode;
+    }
+}
+
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if (head == nullptr) return head;
+
+        ListNode* dummyNode = new ListNode(-1);
+        dummyNode->next = head;
+
+        ListNode *sortNode = head;
+        ListNode *curNode = head->next;
+        while (curNode != nullptr) {
+           if (sortNode->val <= curNode->val) {
+               sortNode = sortNode->next;
+           } else {
+               ListNode *preNode = dummyNode;
+               while (preNode->next->val <= curNode->val) {
+                   preNode = preNode->next;
+               }
+               sortNode->next = curNode->next;
+               curNode->next = preNode->next;
+               preNode->next = curNode;
+           }
+
+           curNode = sortNode->next;
+        }
+
+        return dummyNode->next;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    int arr[] = {4, 2, 1, 3};
+    int n = sizeof(arr) / sizeof(int);
+
+    ListNode *head = createListNode(arr, n);
+    printListNode(head);
+
+    ListNode *retNode = Solution().insertionSortList(head);
+    printListNode(retNode);
+
+    deleteListNode(retNode);
+    return 0;
+}
