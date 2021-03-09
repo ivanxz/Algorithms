189f817651b47150a391a9c902c4005f2c6caa31
Ivan_XZ <xz1051708973@163.com>
2021-03-09 17:47:37 +0800

    feat: add 148-sortList

--- src/leetcode/02-list/148-sortList/main.cpp
+++ src/leetcode/02-list/148-sortList/main.cpp
@@ -54,18 +54,49 @@ void deleteListNode(ListNode *head) {
 class Solution {
 public:
     ListNode *sortList(ListNode *head) {
-        if (head == nullptr) return head;
+        return sortList(head, nullptr);
+    }
 
-        ListNode* dummyNode = new ListNode(-1);
-        dummyNode->next = head;
-
-        ListNode *tailNode = head;
-        ListNode *curNode = head->next;
-        while (curNode != nullptr) {
-
+private:
+    ListNode* sortList(ListNode* head, ListNode* tail) {
+        if (head == nullptr) {
+            return head;
         }
+        if (head->next == tail) {
+            head->next = nullptr;
+            return head;
+        }
+        ListNode* slow = head, *fast = head;
+        while (fast != tail) {
+            slow = slow->next;
+            fast = fast->next;
+            if (fast != tail) {
+                fast = fast->next;
+            }
+        }
+        ListNode* mid = slow;
+        return merge(sortList(head, mid), sortList(mid, tail));
+    }
 
-        return dummyNode->next;
+    ListNode* merge(ListNode* head1, ListNode* head2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* temp = dummyHead, *temp1 = head1, *temp2 = head2;
+        while (temp1 != nullptr && temp2 != nullptr) {
+            if (temp1->val <= temp2->val) {
+                temp->next = temp1;
+                temp1 = temp1->next;
+            } else {
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if (temp1 != nullptr) {
+            temp->next = temp1;
+        } else if (temp2 != nullptr) {
+            temp->next = temp2;
+        }
+        return dummyHead->next;
     }
 };
 
