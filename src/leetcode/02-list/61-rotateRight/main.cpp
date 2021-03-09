df4a81b961aa69f14d4d1005961ac2fe11a7dd57
Ivan_XZ <xz1051708973@163.com>
2021-03-09 15:38:31 +0800

    feat: add 61-rotateRight

--- src/leetcode/02-list/61-rotateRight/main.cpp
+++ src/leetcode/02-list/61-rotateRight/main.cpp
@@ -54,7 +54,24 @@ void deleteListNode(ListNode *head) {
 class Solution {
 public:
     ListNode *rotateRight(ListNode *head, int k) {
+        if (head == nullptr || head->next == nullptr) return head;
 
+        ListNode *oldTail = head;
+        int n = 1;
+        while (oldTail->next != nullptr) {
+            oldTail = oldTail->next;
+            n++;
+        }
+
+        oldTail->next = head;
+
+        ListNode *newTail = head;
+        for (int i = 0; i < n - k % n - 1; i++)
+            newTail = newTail->next;
+
+        ListNode *newHead = newTail->next;
+        newTail->next = nullptr;
+        return newHead;
     }
 };
 
