3a5804751c4a4e780f74ac2b9fa831661144f280
Ivan_XZ <xz1051708973@163.com>
2021-03-10 11:53:59 +0800

    feat: add 1-twoSum

--- src/leetcode/search/1-twoSum/main.cpp
+++ src/leetcode/search/1-twoSum/main.cpp
@@ -4,17 +4,34 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    static vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> record;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = record.find(target - nums[i]);
+            if (it != record.end()) {
+                return {it->second, i};
+            } else {
+                record[nums[i]] = i;
+            }
+        }
 
+        return {};
     }
 };
 
 int main(int argc, char *argv[]) {
+    vector<int> nums = {3, 2, 3};
+    int target = 6;
+    vector<int> ret = Solution().twoSum(nums, target);
+    for (auto k : ret) {
+        cout << k << " ";
+    }
     cout << endl;
     return 0;
 }
