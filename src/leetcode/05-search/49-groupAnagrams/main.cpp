32f783cbc95ab865c9fea9fd5c2008b13998d48e
Ivan_XZ <xz1051708973@163.com>
2021-03-11 19:55:21 +0800

    feat: add 49-groupAnagrams

--- src/leetcode/05-search/49-groupAnagrams/main.cpp
+++ src/leetcode/05-search/49-groupAnagrams/main.cpp
@@ -4,13 +4,28 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs) {
+        vector<vector<string>> res;
+        unordered_map<string, int> record;
+        for (int i = 0; i < strs.size(); ++i) {
+            string str = strs[i];
+            sort(str.begin(), str.end());
+            if (!record.count(str)) {
+                vector<string> vec = {strs[i]};
+                res.push_back(vec);
+                record[str] = res.size() - 1;
+            } else {
+                res[record[str]].push_back(strs[i]);
+            }
+        }
 
+        return res;
     }
 };
 
