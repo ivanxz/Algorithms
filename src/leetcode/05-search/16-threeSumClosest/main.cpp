f5fa2bbd759d8efc1a6ca365fc07149d1c32d9e0
Ivan_XZ <xz1051708973@163.com>
2021-03-11 16:52:13 +0800

    feat: add 16-threeSumClosest

--- src/leetcode/05-search/16-threeSumClosest/main.cpp
+++ src/leetcode/05-search/16-threeSumClosest/main.cpp
@@ -9,12 +9,34 @@ using namespace std;
 
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
+    int threeSumClosest(vector<int> &nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = (int) nums.size();
+        int closestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < n - 2; ++i) {
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (abs(sum - target) < abs(closestSum - target)) {
+                    closestSum = sum;
+                }
 
+                if (sum < target) l++;
+                else if (sum > target) r--;
+                else {
+                    return target;
+                }
+            }
+        }
+
+        return closestSum;
     }
 };
 
 int main(int argc, char *argv[]) {
-    cout << endl;
+    vector<int> nums = {1,2,4,8,16,32,64,128};
+    int target = 82;
+    cout << Solution().threeSumClosest(nums, target) << endl;
     return 0;
 }
