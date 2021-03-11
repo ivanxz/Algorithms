008fc48115de5b9e97bad53b26488cbdd5c3d5c1
Ivan_XZ <xz1051708973@163.com>
2021-03-11 19:22:48 +0800

    feat: add 202-isHappy

--- src/leetcode/05-search/202-isHappy/main.cpp
+++ src/leetcode/05-search/202-isHappy/main.cpp
@@ -4,17 +4,52 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
-    static bool isHappy(int n) {
+//    unordered_set<int> history;
+//
+//    bool isHappy(int n) {
+//        if (n == 1)
+//            return true;
+//        if (history.count(n))
+//            return false;
+//
+//        history.insert(n);
+//        int tmp = 0;
+//        while (n != 0) {
+//            tmp += (n % 10) * (n % 10);
+//            n /= 10;
+//        }
+//
+//        return isHappy(tmp);
+//    }
 
+    bool isHappy(int n) {
+        unordered_set<int> record;
+        while (n != 1 && !record.count(n)) {
+            record.insert(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+
+    int getNext(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+
+        return sum;
     }
 };
 
 int main(int argc, char *argv[]) {
-    cout << endl;
+    cout << Solution().isHappy(19) << endl;
     return 0;
 }
