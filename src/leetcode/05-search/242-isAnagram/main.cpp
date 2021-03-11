efce52477ec983755dcbcee2499c694c86f692ff
Ivan_XZ <xz1051708973@163.com>
2021-03-11 18:50:44 +0800

    feat: add 242-isAnagram

--- src/leetcode/05-search/242-isAnagram/main.cpp
+++ src/leetcode/05-search/242-isAnagram/main.cpp
@@ -4,17 +4,47 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    static bool isAnagram(string s, string t) {
+    bool isAnagram(string s, string t) {
+//        unordered_map<char, int> record;
+//        for (auto c: s) {
+//            record[c]++;
+//        }
+//
+//        for (auto c: t) {
+//            if (record.count(c)) {
+//                record[c]--;
+//                if (0 == record[c]) record.erase(c);
+//            } else {
+//                return false;
+//            }
+//        }
+//
+//        return record.empty();
 
+        if (s.length() != t.length())
+            return false;
+
+        int freq[26] = {};
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++)
+            if (freq[i] != 0)
+                return false;
+
+        return true;
     }
 };
 
 int main(int argc, char *argv[]) {
-    cout << endl;
+    cout << Solution().isAnagram("anagram", "nagaram") << endl;
     return 0;
 }
--- src/leetcode/05-search/49-groupAnagrams/main.cpp
+++ src/leetcode/05-search/49-groupAnagrams/main.cpp
@@ -9,12 +9,20 @@ using namespace std;
 
 class Solution {
 public:
-    static vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string> &strs) {
 
     }
 };
 
 int main(int argc, char *argv[]) {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    vector<vector<string>> res = Solution().groupAnagrams(strs);
+    for (const auto &vec: res) {
+        for (const string &k : vec) {
+            cout << k << " ";
+        }
+        cout << endl;
+    }
     cout << endl;
     return 0;
 }
