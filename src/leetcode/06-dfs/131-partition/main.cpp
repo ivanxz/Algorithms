2379b0e0a37f5930b6d80475849d3bd8304cb872
Ivan_XZ <xz1051708973@163.com>
2021-03-21 00:55:55 +0800

    feat: add 131-partition

--- src/leetcode/06-dfs/131-partition/main.cpp
+++ src/leetcode/06-dfs/131-partition/main.cpp
@@ -10,30 +10,42 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        dfs(s, 0);
+        n = s.size();
+        f.assign(n, vector<int>(n));
+        vector<string> path;
+        dfs(s, 0, path);
         return res;
     }
 
 private:
+    vector<vector<int>> f;
     vector<vector<string>> res;
+    int n;
 
-    void dfs(const string& s, int index) {
-
-
-        for (int i = index; i < s.length(); ++i) {
-
+    void dfs(const string &s, int i, vector<string>& path) {
+        if (i == n) {
+            res.push_back(path);
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (isPalindrome(s, i, j) == 1) {
+                path.push_back(s.substr(i, j - i + 1));
+                dfs(s, j + 1, path);
+                path.pop_back();
+            }
         }
     }
 
-    bool isMirror(const string& s) {
-        if (1 == s.length()) return true;
-
-        int l = 0, r = s.length() - 1;
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
+    // 记忆化搜索中，f[i][j] = 0 表示未搜索，1 表示是回文串，-1 表示不是回文串
+    int isPalindrome(const string &s, int i, int j) {
+        if (f[i][j]) {
+            return f[i][j];
+        }
+        if (i >= j) {
+            return f[i][j] = 1;
         }
 
-        return true;
+        return f[i][j] = (s[i] == s[j] ? isPalindrome(s, i + 1, j - 1) : -1);
     }
 };
 
