48fae7ec1ef9421ee263bb43fd6e4ff06888d5c9
Ivan_XZ <xz1051708973@163.com>
2021-03-21 00:36:51 +0800

    feat: add 401-readBinaryWatch

--- src/leetcode/06-dfs/401-readBinaryWatch/main.cpp
+++ src/leetcode/06-dfs/401-readBinaryWatch/main.cpp
@@ -10,13 +10,32 @@ using namespace std;
 class Solution {
 public:
     vector<string> readBinaryWatch(int num) {
+        dfs(num, 0, 0, 0);
+        return res;
+    }
 
+private:
+    vector<string> res;
+    const int hourLed[10]{1, 2, 4, 8, 0, 0, 0, 0, 0, 0};
+    const int minuteLed[10]{0, 0, 0, 0, 1, 2, 4, 8, 16, 32};
+
+    void dfs(int num, int index, int hour, int minute) {
+        if (hour > 11 || minute > 59) return;
+        if (num == 0) {
+            string time = to_string(hour) + ":" + (minute < 10 ? "0" + to_string(minute) : to_string(minute));
+            res.push_back(time);
+            return;
+        }
+
+        for (int i = index; i < 10; ++i) {
+            dfs(num - 1, i + 1, hour + hourLed[i], minute + minuteLed[i]);
+        }
     }
 };
 
 int main(int argc, char *argv[]) {
     vector<string> res = Solution().readBinaryWatch(1);
-    for (auto& str : res) {
+    for (auto &str : res) {
         cout << str << " ";
     }
     cout << endl;
